24959cacd96aac5bec7b3ced0c69c06501b170ab
Levente Berky <levente.berky@gmail.com>
2020-12-14 14:03:23 +0100

    Convert Clusters page to use the API hook

--- src/Containers/Clusters/Clusters.js
+++ src/Containers/Clusters/Clusters.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
 import { useQueryParams } from '../../Utilities/useQueryParams';
+import useApi from '../../Utilities/useApi';
 
 import styled from 'styled-components';
 import LoadingState from '../../Components/LoadingState';
@@ -58,13 +59,13 @@ const timeFrameOptions = [
     { value: 31, label: 'Past month', disabled: false }
 ];
 
-function formatClusterName(data) {
+const clusterOptionsMapper = ({ templates: data }) => {
     const defaultClusterOptions = [
         { value: 'please choose', label: 'Select cluster', disabled: true },
         { value: 'all', label: 'All clusters', disabled: false }
     ];
     return data.reduce(
-        (formatted, { label, cluster_id: id, install_uuid: uuid }) => {
+        (formatted, { label, clusterId: id, installUuid: uuid }) => {
             if (label.length === 0) {
                 formatted.push({ value: id, label: uuid, disabled: false });
             } else {
@@ -75,7 +76,11 @@ function formatClusterName(data) {
         },
         defaultClusterOptions
     );
-}
+};
+
+const chartMapper = ({ data = []}) => data;
+const modulesMapper = ({ modules = []}) => modules;
+const templatesMapper = ({ templates = []}) => templates;
 
 const initialQueryParams = {
     startDate: moment()
@@ -86,14 +91,13 @@ const initialQueryParams = {
 
 const Clusters = ({ history }) => {
     const [ preflightError, setPreFlightError ] = useState(null);
-    const [ barChartData, setBarChartData ] = useState([]);
-    const [ lineChartData, setLineChartData ] = useState([]);
-    const [ templatesData, setTemplatesData ] = useState([]);
-    const [ modulesData, setModulesData ] = useState([]);
-    const [ clusterOptions, setClusterOptions ] = useState([]);
+    const [ chartData, loadChartData ] = useApi([], chartMapper);
+    const [ templatesData, loadTemplatesData ] = useApi([], templatesMapper);
+    const [ modulesData, loadModulesData ] = useApi([], modulesMapper);
+    const [ clusterOptions, loadClusterOptions ] = useApi([], clusterOptionsMapper);
+
     const [ clusterTimeFrame, setClusterTimeFrame ] = useState(31);
     const [ selectedCluster, setSelectedCluster ] = useState('all');
-    const [ isLoading, setIsLoading ] = useState(true);
     const { queryParams, setEndDate, setStartDate, setId } = useQueryParams(
         initialQueryParams
     );
@@ -101,40 +105,45 @@ const Clusters = ({ history }) => {
     useEffect(() => {
         insights.chrome.appNavClick({ id: 'clusters', secondaryNav: true });
 
-        setIsLoading(true);
         window.insights.chrome.auth.getUser().then(() =>
-            preflightRequest().then(() =>
-                readClusters().then(({ templates: clustersData = []}) => {
-                    const clusterOptions = formatClusterName(clustersData);
-                    setClusterOptions(clusterOptions);
-                    setIsLoading(false);
-                })
-            ).catch((error) => {
+            preflightRequest().catch((error) => {
                 setPreFlightError({ preflightError: error });
+            }).then(() => {
+                loadClusterOptions(readClusters());
             })
         );
+
     }, []);
 
     // Get and update the data
     useEffect(() => {
-        setIsLoading(true);
-        window.insights.chrome.auth.getUser().then(() =>
-            Promise.all([
-                readChart30({ params: queryParams }),
-                readModules({ params: queryParams }),
-                readTemplates({ params: queryParams })
-            ]).then(([
-                { data: chartData = []},
-                { modules: modulesData = []},
-                { templates: templatesData = []}
-            ]) => {
-                queryParams.id ? setLineChartData(chartData) : setBarChartData(chartData);
-                setModulesData(modulesData);
-                setTemplatesData(templatesData);
-                setIsLoading(false);
-            }).catch(() => [])
-        );
+        loadChartData(readChart30({ params: queryParams }));
+        loadModulesData(readModules({ params: queryParams }));
+        loadTemplatesData(readTemplates({ params: queryParams }));
     }, [ queryParams ]);
+
+    const displayChart = () => {
+        if (chartData.isLoading) {
+            return <LoadingState />;
+        } else if (chartData.data.length > 0) {
+            if (queryParams.id) {
+                return <LineChart
+                    margin={ { top: 20, right: 20, bottom: 50, left: 70 } }
+                    id="d3-line-chart-root"
+                    data={ chartData.data }
+                    value={ clusterTimeFrame }
+                    clusterId={ queryParams.id }
+                />;
+            } else {
+                return <BarChart
+                    margin={ { top: 20, right: 20, bottom: 50, left: 70 } }
+                    id="d3-bar-chart-root"
+                    data={ chartData.data }
+                    value={ clusterTimeFrame }
+                />;
+            }
+        }
+    };
 
     return (
         <React.Fragment>
@@ -147,106 +156,85 @@ const Clusters = ({ history }) => {
                 </Main>
             ) }
             { !preflightError && (
-        <>
-          <Main style={ { paddingBottom: '0' } }>
-              <Card>
-                  <CardTitle style={ { paddingBottom: '0', paddingTop: '0' } }>
-                      <h2>
-                          <FilterIcon style={ { marginRight: '10px' } } />
-                  Filter
-                      </h2>
-                      <div style={ { display: 'flex', justifyContent: 'flex-end' } }>
-                          <FormSelect
-                              name="selectedCluster"
-                              value={ selectedCluster }
-                              onChange={ value => {
-                                  setSelectedCluster(value);
-                                  setId(value);
-                              } }
-                              aria-label="Select Cluster"
-                              style={ { margin: '2px 10px' } }
-                          >
-                              { clusterOptions.map(({ value, label, disabled }, index) => (
-                                  <FormSelectOption
-                                      isDisabled={ disabled }
-                                      key={ index }
-                                      value={ value }
-                                      label={ label }
-                                  />
-                              )) }
-                          </FormSelect>
-                          <FormSelect
-                              name="clusterTimeFrame"
-                              value={ clusterTimeFrame }
-                              onChange={ value => {
-                                  setClusterTimeFrame(+value);
-                                  setEndDate();
-                                  setStartDate(+value);
-                              } }
-                              aria-label="Select Date Range"
-                              style={ { margin: '2px 10px' } }
-                          >
-                              { timeFrameOptions.map((option, index) => (
-                                  <FormSelectOption
-                                      isDisabled={ option.disabled }
-                                      key={ index }
-                                      value={ option.value }
-                                      label={ option.label }
-                                  />
-                              )) }
-                          </FormSelect>
-                      </div>
-                  </CardTitle>
-              </Card>
-          </Main>
-          <Main>
-              <Card>
-                  <PFCardTitle>
-                      <h2>Job status</h2>
-                  </PFCardTitle>
-                  <CardBody>
-                      { isLoading && !preflightError && <LoadingState /> }
-                      { selectedCluster === 'all' &&
-                  barChartData.length > 0 &&
-                  !isLoading && (
-                          <BarChart
-                              margin={ { top: 20, right: 20, bottom: 50, left: 70 } }
-                              id="d3-bar-chart-root"
-                              data={ barChartData }
-                              value={ clusterTimeFrame }
-                          />
-                      ) }
-                      { selectedCluster !== 'all' &&
-                  lineChartData.length > 0 &&
-                  !isLoading && (
-                          <LineChart
-                              margin={ { top: 20, right: 20, bottom: 50, left: 70 } }
-                              id="d3-line-chart-root"
-                              data={ lineChartData }
-                              value={ clusterTimeFrame }
-                              clusterId={ queryParams.id }
-                          />
-                      ) }
-                  </CardBody>
-              </Card>
-              <div
-                  className="dataCard"
-                  style={ { display: 'flex', marginTop: '20px' } }
-              >
-                  <TemplatesList
-                      history={ history }
-                      queryParams={ queryParams }
-                      clusterId={ queryParams.id }
-                      templates={ templatesData.slice(0, 10) }
-                      isLoading={ isLoading }
-                  />
-                  <ModulesList
-                      modules={ modulesData.slice(0, 10) }
-                      isLoading={ isLoading }
-                  />
-              </div>
-          </Main>
-        </>
+                <React.Fragment>
+                    <Main style={ { paddingBottom: '0' } }>
+                        <Card>
+                            <CardTitle style={ { paddingBottom: '0', paddingTop: '0' } }>
+                                <h2>
+                                    <FilterIcon style={ { marginRight: '10px' } } />
+                                    Filter
+                                </h2>
+                                <div style={ { display: 'flex', justifyContent: 'flex-end' } }>
+                                    <FormSelect
+                                        name="selectedCluster"
+                                        value={ selectedCluster }
+                                        onChange={ value => {
+                                            setSelectedCluster(value);
+                                            setId(value);
+                                        } }
+                                        aria-label="Select Cluster"
+                                        style={ { margin: '2px 10px' } }
+                                    >
+                                        { clusterOptions.data.map(({ value, label, disabled }, index) => (
+                                            <FormSelectOption
+                                                isDisabled={ disabled }
+                                                key={ index }
+                                                value={ value }
+                                                label={ label }
+                                            />
+                                        )) }
+                                    </FormSelect>
+                                    <FormSelect
+                                        name="clusterTimeFrame"
+                                        value={ clusterTimeFrame }
+                                        onChange={ value => {
+                                            setClusterTimeFrame(+value);
+                                            setEndDate();
+                                            setStartDate(+value);
+                                        } }
+                                        aria-label="Select Date Range"
+                                        style={ { margin: '2px 10px' } }
+                                    >
+                                        { timeFrameOptions.map((option, index) => (
+                                            <FormSelectOption
+                                                isDisabled={ option.disabled }
+                                                key={ index }
+                                                value={ option.value }
+                                                label={ option.label }
+                                            />
+                                        )) }
+                                    </FormSelect>
+                                </div>
+                            </CardTitle>
+                        </Card>
+                    </Main>
+                    <Main>
+                        <Card>
+                            <PFCardTitle>
+                                <h2>Job status</h2>
+                            </PFCardTitle>
+                            <CardBody>
+                                { displayChart() }
+                            </CardBody>
+                        </Card>
+                        <div
+                            className="dataCard"
+                            style={ { display: 'flex', marginTop: '20px' } }
+                        >
+                            <TemplatesList
+                                history={ history }
+                                queryParams={ queryParams }
+                                clusterId={ queryParams.id }
+                                templates={ templatesData.data.slice(0, 10) }
+                                isLoading={ templatesData.isLoading }
+                            />
+                            <ModulesList
+                                modules={ modulesData.data.slice(0, 10) }
+                                isLoading={ modulesData.isLoading }
+                            />
+                        </div>
+                    </Main>
+                </React.Fragment>
             ) }
         </React.Fragment>
     );
